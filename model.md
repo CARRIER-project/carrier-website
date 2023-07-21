c17021d37af2013feed1649bcec17a128d74e1b3
florian <florian.vandaalen@maastro.nl>
2023-07-21 13:05:49 +0200

    Implemented a basic version of calling the webservice

--- model.md
+++ model.md
@@ -4,13 +4,59 @@ layout: default
 
 <script src="script.js"></script>
 
-# Enter Your Age
+# Please provide the following information
 
 <form>
-  <input type="number" id="ageInput" placeholder="Enter your age">
-  <button type="button" onclick="fetchAgeData()">Submit</button>
-  <input type="number" id="weight" placeholder="Enter your weight">
-  <button type="button" onclick="fetchAgeData()">Submit</button>
+Do you use antihypertensives: <input type="radio" name="antihypertensives" id="yes_antihypertensives" value ="true" checked /> Yes <input type="radio" name="antihypertensives" id="no_antihypertensives"/> No
+<form>
+</form>
+Do you use beta blocking agents: <input type="radio" name="beta_blocking_agents" id="yes_beta_blocking_agents" value ="true" checked /> Yes <input type="radio" name="beta_blocking_agents" id="no_beta_blocking_agents"/> No
+<form>
+</form>
+Do you use calcium channel blockers: <input type="radio" name="calcium_channel_blockers" id="yes_calcium_channel_blockers" value ="true" checked /> Yes <input type="radio" name="calcium_channel_blockers" id="no_calcium_channel_blockers"/> No
+<form>
+</form>
+Do you use RAS inhibitors: <input type="radio" name="RAS_inhibitors" id="yes_RAS_inhibitors" value ="true" checked /> Yes <input type="radio" name="RAS_inhibitors" id="no_RAS_inhibitors"/> No
+<form>
+</form>
+Do you use Lipid modifying agents: <input type="radio" name="lipid_modifying_agents" id="yes_lipid_modifying_agents" value ="true" checked /> Yes <input type="radio" name="lipid_modifying_agents" id="no_lipid_modifying_agents"/> No
+<form>
+</form>
+Are you a current smoker: <input type="radio" name="current_smoker" id="yes_current_smoker" value ="true" checked /> Yes <input type="radio" name="current_smoker" id="no_current_smoker"/> No
+<form>
+</form>
+Are you an ex-smoker: <input type="radio" name="ex_smoker" id="yes_ex_smoker" value ="true" checked /> Yes <input type="radio" name="ex_smoker" id="no_ex_smoker"/> No
+<form>
+</form>
+What is your gender: <input type="radio" name="gender" id="male_gender" value ="true" checked />Male <input type="radio" name="gender" id="female_gender"/> Female
+<form>
+</form>
+What is your age: <input type="number" id="ageInput" placeholder="Age">
+<form>
+</form>
+What is your TC (: <input type="number" id="TCInput" placeholder="TC">
+<form>
+</form>
+What is your HDL: <input type="number" id="HDLInput" placeholder="HDL">
+<form>
+</form>
+What is your eetscore: <input type="number" id="eetscoreInput" placeholder="Eetscore">
+<form>
+</form>
+What is your SBP: <input type="number" id="SBPInput" placeholder="SBP">
+<form>
+</form>
+What is your CHAMPS_MVPA_score: <input type="number" id="CHAMPS_MVPA_scoreInput" placeholder="CHAMPS_MVPA_score">
+<form>
+</form>
+What is your postalcode: <input type="string" id="postalcodeInput" placeholder="postalcode">
+<form>
+</form>
+What is your housenumber: <input type="number" id="housenumberInput" placeholder="housenumber">
+<form>
+</form>
+<form>
+  <button type="button" onclick="runModel()">Submit</button>
 </form>
 
 <div id="resultContainer">
--- script.js
+++ script.js
@@ -1,22 +1,84 @@
-function fetchAgeData() {
-  const ageInput = document.getElementById('ageInput').value;
-  const url = 'https://carrier-model-exposer-5cfb51351e5b.herokuapp.com/'; // Replace with the actual API URL
+async function runModel() {
+
+  var antihypertensives = "yes";
+  if(!document.getElementById('yes_antihypertensives').checked){
+      antihypertensives="no";
+  }
+  var beta_blocking_agents = "yes";
+  if(!document.getElementById('yes_beta_blocking_agents').checked){
+      beta_blocking_agents="no";
+  }
+  var calcium_channel_blockers = "yes";
+  if(!document.getElementById('yes_calcium_channel_blockers').checked){
+     calcium_channel_blockers="no";
+  }
+  var RAS_inhibitors ="yes";
+  if(!document.getElementById('yes_RAS_inhibitors').checked){
+      RAS_inhibitors="no";
+  }
+  var lipid_modifying_agents ="yes";
+  if(!document.getElementById('yes_lipid_modifying_agents').checked){
+      lipid_modifying_agents="no";
+  }
+  var current_smoker = "yes";
+  if(!document.getElementById('yes_current_smoker').checked){
+      current_smoker="no";
+  }
+  var ex_smoker = "yes";
+  if(!document.getElementById('yes_ex_smoker').checked){
+    ex_smoker="no";
+  }
+  var gender = "male";
+  if(!document.getElementById('male_gender').checked){
+    gender = "female";
+  }
+  var age = document.getElementById('ageInput').value;
+  var TC = document.getElementById('TCInput').value;
+  var HDL = document.getElementById('HDLInput').value;
+  var eetscore = document.getElementById('eetscoreInput').value;
+  var SBP = document.getElementById('SBPInput').value;
+  var champs = document.getElementById('CHAMPS_MVPA_scoreInput').value;
+  var postalcode = document.getElementById('postalcodeInput').value;
+  var housenumber = document.getElementById('housenumberInput').value;
+  
+  var url = 'http://riskmodel.carrier-mu.src.surf-hosted.nl:443/estimateBaseLineRisk';
 
   // Assuming the API expects an 'age' parameter
-  const requestBody = JSON.stringify({ age: ageInput });
+  var requestBody = "{"+
+                        "\"input\" : {"+
+                       	"\"TC\" : \""+ TC + "\","+
+                       	"\"HDL\" : \""+ HDL + "\","+
+                       	"\"age\" : \""+ age + "\","+
+                        "\"antihypertensives\" : \""+ antihypertensives + "\","+
+                       	"\"beta_blocking_agents\" : \""+ beta_blocking_agents + "\","+
+                        "\"calcium_channel_blockers\" : \""+ calcium_channel_blockers + "\","+
+                        "\"RAS_inhibitors\" : \"" + RAS_inhibitors  + "\","+
+                        "\"lipid_modifying_agents\" : \""+ lipid_modifying_agents +"\","+
+                       	"\"address_postcode\" : \""+ postalcode + "\","+
+                        "\"address_house_number\" : \""+ housenumber + "\","+
+                       	"\"CHAMPS_MVPA_score\" : \"" + champs + "\","+
+                        "\"current_smoker\" : \""+ current_smoker+  "\","+
+                        "\"eetscore\" : \""+ eetscore + "\","+
+                       	"\"ex_smoker\" : \"" + ex_smoker + "\","+
+                        "\"gender\" : \""+ gender + "\","+
+                        "\"SBP\" : \""+ SBP +"\""+
+                        "}"+
+                       "}"
   
-  const response = fetch(url, {
+  const response = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: requestBody,
     });
-
-	displayResult(response);
+	
+	const json = await response.json();
+	displayResult(json["probabilities"]["CVD"]);
 }
 
 function displayResult(data) {
   const resultContainer = document.getElementById('resultContainer');
-  resultContainer.innerHTML = `<h2>Result:</h2><p>${data}</p>`;
+
+  resultContainer.textContent = `Your risk of CVD is ${ data } %`;
 }
