6b829cdc4a24fb7cf6be579cf03512313890e722
florian <florian.vandaalen@maastro.nl>
2023-07-24 13:55:35 +0200

    remove mode

--- script.js
+++ script.js
@@ -66,7 +66,6 @@ async function runModel() {
   
   const response = await fetch(url, {
       method: 'POST',
-	  mode: "cors",
       headers: {
         'Content-Type': 'application/json'
       },
